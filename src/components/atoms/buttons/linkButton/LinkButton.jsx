b5b03e7f7a89cfccd557c65829a392faf1ab4b08
Tayyyyylor <bryan.h.pro@gmail.com>
2024-02-20 10:49:28 +0100

    add img on menu and contact on homepage

--- src/components/atoms/buttons/linkButton/LinkButton.jsx
+++ src/components/atoms/buttons/linkButton/LinkButton.jsx
@@ -1,5 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function LinkButton({ label }) {
-  return <div className="linkButton">{label}</div>
+export default function LinkButton({ label, pictures }) {
+  const [isHover, setIsHover] = useState(false)
+
+  console.log('isHover', isHover)
+
+  return <div className="linkButton"   
+  onMouseEnter={() => setIsHover(true)}
+  onMouseLeave={() => setIsHover(false)}
+  style={{
+    backgroundImage: isHover ? `url(${pictures})` : 'none',
+    backgroundSize: 'cover', // Ajustez si nécessaire
+    backgroundRepeat: 'no-repeat', // Ajustez si nécessaire
+  }}
+  >{label}</div>
 }
--- src/components/templates/homepage/Homepage.jsx
+++ src/components/templates/homepage/Homepage.jsx
@@ -2,16 +2,15 @@ import LinkButton from '@/components/atoms/buttons/linkButton/LinkButton'
 import Contact from '@/components/molecules/contact/Contact'
 import SliderPartner from '@/components/molecules/sliderPartner/SliderPartner'
 import React from 'react'
+import { linkButtonData } from './Homepage.utils'
 
 export default function HomepageTemplate() {
   return (
     <main className="homepage">
       <section className="homepage__button_container">
-        <LinkButton label="tournage vidéos" />
-        <LinkButton label="votre évènement" />
-        <LinkButton label="réalisations" />
-        <LinkButton label="création graphique" />
-        <LinkButton label="location matériel" />
+      {linkButtonData.map((data, index) => (
+        <LinkButton key={index} label={data.label} pictures={data.pictures}/>
+      ))}
       </section>
       <section className="homepage__slider">
         <SliderPartner />
